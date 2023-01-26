887a57fc9d3659237b2416d533cc8c6247eddd62
Luis Muñoz <l.munoz.m@outlook.com>
2023-01-26 14:22:47 -0300

    Fix linter errors

--- decoder.rb
+++ decoder.rb
@@ -37,31 +37,32 @@ $decode_hash = {
 '----.' => '9'
 }
 
+
 def decode_char(message)
-    return $decode_hash[message]
+  return $decode_hash[message]
 end
     
 def decode_word(message)
-    result = ""
-    word = message.split(' ')
-    word.each do |char|
-        result = "#{result}#{decode_char(char)}"
-    end
-    return result
+  result = ""
+  word = message.split(' ')
+  word.each do |char|
+    result = "#{result}#{decode_char(char)}"
+  end
+  return result
 end
 
 def decode(message)
-    #split by 3 spaces 
-    #call decode_word with each word    
-    words = message.split('  ')
-    decoded_message = ''
-    words.each do |word|
-        decoded_word = decode_word(word)
-        decoded_message = "#{decoded_message}#{decoded_word} "
+  #split by 3 spaces 
+  #call decode_word with each word    
+  words = message.split('  ')
+  decoded_message = ''
+  words.each do |word|
+    decoded_word = decode_word(word)
+    decoded_message = "#{decoded_message}#{decoded_word} "
   end
   decoded_message.strip
 end
 
 print decode_word("-- -.--")
-puts decode('-- -.--   -. .- -- .')
-puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+puts decode("-- -.--   -. .- -- .")
+puts decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
